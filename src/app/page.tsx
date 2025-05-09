8f6a0ab9bae6ec6513c8b0e72c51646cb41cfd02
HasiniWij <hasiniwij@gmail.com>
2025-05-09 13:34:38 +0530

    fix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-
-import Spline from '@splinetool/react-spline';
+import SplineWrapper from "@/components/ui/splineWrapper";
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
-      <Spline scene="https://prod.spline.design/2BYPhGAm9V-fx62e/scene.splinecode" />
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <SplineWrapper />
     </div>
-    
   );
 }
---
+++ src/components/ui/splineWrapper.tsx
@@ -0,0 +1,9 @@
+// components/SplineWrapper.tsx
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function SplineWrapper() {
+  return (
+    <Spline scene="https://prod.spline.design/2BYPhGAm9V-fx62e/scene.splinecode" />
+  );
+}
